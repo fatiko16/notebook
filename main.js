05ae4354166b517497ea3dba99c61b8c41617676
Fatih <fatihealan@gmail.com>
2021-01-20 14:42:36 -0500

    Used javascript for putting navbar to each page, changed some basic design

---
+++ main.js
@@ -0,0 +1,16 @@
+let navBar = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="dailyplan.html">Daily Plan</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="goals.html">Goals</a>
+        </li>
+    </ul>
+</div>
+</nav>`;
+document.getElementById("navbar").innerHTML = navBar
